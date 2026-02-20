73620719b62ee395c435dc6a2670e9a171e3e522
Azhar <azhar.subair.2@gmail.com>
2026-02-21 00:20:04 +0530

    router back to home on back

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 
+// 👇 Add here (outside render)
+let lastTouchEnd = 0;
+
+document.addEventListener("touchend", function (event) {
+  const now = Date.now();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, { passive: false });
+
+// 👇 Only JSX inside render
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+  </StrictMode>
 )
